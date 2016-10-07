109c106c975dd00ba4eb600bf187bfaa604aa0d5
volodymyr4509 <volodymyr4509@gmail.com>
2016-10-07 13:48:53 +0300

    Added simple training exercise view

--- src/main/webapp/app/training/training.controller.js
+++ src/main/webapp/app/training/training.controller.js
@@ -5,20 +5,23 @@
         .module('mongoExApp')
         .controller('TrainingController', TrainingController);
 
-    TrainingController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    TrainingController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Exercise'];
 
-    function TrainingController ($scope, Principal, LoginService, $state) {
+    function TrainingController ($scope, Principal, LoginService, $state, Exercise) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
+        vm.exercises = [];
+
         $scope.$on('authenticationSuccess', function() {
             getAccount();
         });
 
         getAccount();
+        loadAllExercises();
 
         function getAccount() {
             Principal.identity().then(function(account) {
@@ -28,6 +31,13 @@
         }
         function register () {
             $state.go('register');
+        }
+
+        function loadAllExercises() {
+            Exercise.query(function(result) {
+                vm.exercises = result;
+                console.log(result);
+            });
         }
     }
 })();
